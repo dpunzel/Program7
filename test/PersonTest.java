db7a4dafdc284564e0b7b993519e673fba9fbdd3
Brian Snider <sniderbr@gmail.com>
2016-03-03 10:59:07 -0800

    update Person unit tests
    Coded in-browser; not tested!  Should be close enough for you to get the general idea.

--- test/PersonTest.java
+++ test/PersonTest.java
@@ -10,47 +10,77 @@ public class PersonTest extends TestCase
     public static final String EXPECTED_NAME = "Bob Smith";
     public static final String EXPECTED_PHONE = "504-555-5555";
     public static final String EXPECTED_EMAIL = "david@dad.com";
-
+    public static final String EXPECTED_TOSTRING = "Name: " + EXPECTED_NAME + " ";
+    
+    private Person _person1;
+    private Person _person2;
+    
+    /**
+     * @see http://junit.sourceforge.net/javadoc/org/junit/Before.html
+     */
+    @Before
+    public void initialize()
+    {
+        _person1 = new Person(EXPECTED_NAME, EXPECTED_PHONE, EXPECTED_EMAIL);
+        _person2 = new Person("", "", "");
+    }
+    
+    /**
+     * @see http://junit.sourceforge.net/javadoc/org/junit/Test.html
+     */
+    @Test
     public void testConstructor() throws Exception
     {
-        final Person person = new Person("Bob Smith", "504-555-5555", "david@dad.com");
-        Assert.assertEquals(EXPECTED_NAME, person.getName());
-        Assert.assertEquals(EXPECTED_PHONE, person.getPhone());
-        Assert.assertEquals(EXPECTED_EMAIL, person.getEmail());
+        Assert.assertNotNull(_person);
+        Assert.assertTrue(_person instanceof Person);
+        this.testGetName();
+        this.testGetPhone();
+        this.testGetEmail();
     }
 
+    @Test
     public void testGetName() throws Exception
     {
-
+        Assert.assertEquals(_person1.getName(), EXPECTED_NAME);
     }
 
+    @Test
     public void testGetPhone() throws Exception
     {
-
+        Assert.assertEquals(_person1.getPhone(), EXPECTED_PHONE);
     }
 
+    @Test
     public void testGetEmail() throws Exception
     {
-
+        Assert.assertEquals(_person1.getEmail(), EXPECTED_EMAIL);
     }
 
-    public void testSetName(String harry) throws Exception
+    @Test
+    public void testSetName() throws Exception
     {
-        this.testSetName("Harry");
+        _person2.setName(EXPECTED_NAME);
+        Assert.assertEquals(_person2.getName(), EXPECTED_NAME);
     }
 
-    public void testSetPhone(String s) throws Exception
+    @Test
+    public void testSetPhone() throws Exception
     {
-        this.testSetPhone("555-555-5555");
+        _person2.setPhone(EXPECTED_PHONE);
+        Assert.assertEquals(_person2.getPhone(), EXPECTED_PHONE);
     }
 
-    public void testSetEmail(String emai) throws Exception
+    @Test
+    public void testSetEmail() throws Exception
     {
-        this.testSetEmail("test@test.com");
+        _person2.setEmail(EXPECTED_EMAIL);
+        Assert.assertEquals(_person2.getEmail(), EXPECTED_EMAIL);
     }
 
+    @Test
     public void testToString() throws Exception
     {
-
+        Assert.assertEquals(_person1.toString(), EXPECTED_TOSTRING);
     }
+    
 }
